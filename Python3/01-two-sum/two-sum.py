5953c85ea3ad0c8a56027a55740454b628572f21
voitenko-lex <3534286+voitenko-lex@users.noreply.github.com>
2020-01-03 21:41:27 +0300

    Second solution v00

--- Python3/01-two-sum/two-sum.py
+++ Python3/01-two-sum/two-sum.py
@@ -5,16 +5,24 @@ import unittest
 
 class Solution:
 #   First Solution  
-    def twoSum(self, nums, target):
-        tnums = tuple(nums)
-        for first_el, first_el_val in enumerate(tnums):
-            for second_el, second_el_val in enumerate(tnums[first_el+1:]):
-                if first_el_val + second_el_val == target:
-                    return [first_el, first_el + 1 + second_el]
+    # def twoSum(self, nums, target):
+    #     tnums = tuple(nums)
+    #     for first_el, first_el_val in enumerate(tnums):
+    #         for second_el, second_el_val in enumerate(tnums[first_el+1:]):
+    #             if first_el_val + second_el_val == target:
+    #                 return [first_el, first_el + 1 + second_el]
     
 #   Second Solution 
-    # def twoSum(self, nums, target):
-    #     pass
+    def twoSum(self, nums, target):
+        dnums = {y:x for x,y in enumerate(nums)}
+        print(nums)
+        print(dnums)
+        for first_el, first_el_val in enumerate(nums):
+            complement = dnums.get(target - first_el_val)
+            if complement != None and complement != first_el_val:
+                return [first_el, complement]
+        
+        raise AttributeError("No two sum solution")
 
 
 class TestStringMethods(unittest.TestCase):
@@ -24,6 +32,12 @@ class TestStringMethods(unittest.TestCase):
         test_input_nums = [2, 7, 11, 15]
         test_input_target = 9
         test_output = [0, 1]
+        self.assertEqual(self.sol.twoSum(test_input_nums, test_input_target), test_output)
+
+    def test_sample01(self):
+        test_input_nums = [3,2,4]
+        test_input_target = 6
+        test_output = [1, 2]
         self.assertEqual(self.sol.twoSum(test_input_nums, test_input_target), test_output)
 
 if __name__ == '__main__':
