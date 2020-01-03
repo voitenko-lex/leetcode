d34bd4a2dae41c5b98fe14f57739c0bcf51f4326
voitenko-lex <3534286+voitenko-lex@users.noreply.github.com>
2020-01-03 13:48:04 +0300

    First Solution

---
+++ Python3/01-two-sum/two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+#   First Solution  
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     tnums = tuple(nums)
+    #     for first_el, first_el_val in enumerate(tnums):
+    #         for second_el, second_el_val in enumerate(tnums[first_el+1:]):
+    #             if first_el_val + second_el_val == target:
+    #                 return [first_el, first_el + 1 + second_el]
+    
+#   Second Solution 
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
