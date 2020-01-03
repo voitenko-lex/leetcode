db1065b1880df295b4f7d157dd59078d56b7cd20
voitenko-lex <3534286+voitenko-lex@users.noreply.github.com>
2020-01-03 14:40:42 +0300

    test_sample00 added

--- Python3/01-two-sum/two-sum.py
+++ Python3/01-two-sum/two-sum.py
@@ -1,12 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+        
+import unittest
+
 class Solution:
 #   First Solution  
-    # def twoSum(self, nums: List[int], target: int) -> List[int]:
-    #     tnums = tuple(nums)
-    #     for first_el, first_el_val in enumerate(tnums):
-    #         for second_el, second_el_val in enumerate(tnums[first_el+1:]):
-    #             if first_el_val + second_el_val == target:
-    #                 return [first_el, first_el + 1 + second_el]
+    def twoSum(self, nums, target):
+        tnums = tuple(nums)
+        for first_el, first_el_val in enumerate(tnums):
+            for second_el, second_el_val in enumerate(tnums[first_el+1:]):
+                if first_el_val + second_el_val == target:
+                    return [first_el, first_el + 1 + second_el]
     
 #   Second Solution 
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+    # def twoSum(self, nums, target):
+    #     pass
+
+
+class TestStringMethods(unittest.TestCase):
+    sol = Solution()
+
+    def test_sample00(self):
+        test_input_nums = [2, 7, 11, 15]
+        test_input_target = 9
+        test_output = [0, 1]
+        self.assertEqual(self.sol.twoSum(test_input_nums, test_input_target), test_output)
+
+if __name__ == '__main__':
+    unittest.main()
