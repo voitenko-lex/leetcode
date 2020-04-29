39b3db19d0adc64df758b40dcbda7fd445298bb9
voitenko-lex <3534286+voitenko-lex@users.noreply.github.com>
2020-04-29 18:51:16 +0300

    bad version

--- Python3/06-zigzag-conversion/zigzag-conversion.py
+++ Python3/06-zigzag-conversion/zigzag-conversion.py
@@ -28,13 +28,57 @@ A   L S  I G
 Y A   H R
 P     I
 
+
+
 """
 import unittest
 
 class Solution:
-    # def convert(self, s: str, numRows: int) -> str:
-    def convert(self, s, numRows):
-        pass
+    def convert(self, s: str, numRows: int) -> str:
+        lstResult = []
+        strResult = ""
+        substr = s
+
+        while True:
+            if len(substr)>numRows:
+                lstResult.append(list(substr[:numRows]))
+                substr = substr[numRows:]
+            elif len(substr)>0:
+                lstResult.append(list(substr))
+                substr = ""
+            else:
+                break
+
+            if len(substr)>(numRows-2):
+                if numRows>1:
+                    lstResult.append([str('')] + list(substr[:(numRows-2)][::-1]))
+                else:
+                    lstResult.append(list(substr[:(numRows-2)][::-1]))          
+                substr = substr[(numRows-2):]
+            elif len(substr)>0:
+                if numRows>1:
+                    lstResult.append([str('')] + list(substr[::-1]))
+                else:
+                    lstResult.append(list(substr[::-1]))
+
+                substr = ""
+            else:
+                break
+
+        for i in range(numRows):
+            for j in range(len(lstResult)):
+                try:
+                    # print(f"j = {j}, i = {i} {str(lstResult[j][i])}")
+                    strResult += str(lstResult[j][i])
+                except IndexError:
+                    pass
+
+
+        print(f"lstResult = {lstResult} len:{len(lstResult)}")
+        print(f"strResult = {strResult}")
+
+        return strResult
+
 
 class TestMethods(unittest.TestCase):
     sol = Solution()
@@ -45,8 +89,16 @@ class TestMethods(unittest.TestCase):
     def test_sample01(self):
         self.assertEqual("PINALSIGYAHRPI", self.sol.convert(s = "PAYPALISHIRING", numRows = 4))
 
+    def test_sample02(self):
+        self.assertEqual("ABC", self.sol.convert(s = "ABC", numRows = 1))
+
+    def test_sample03(self):
+        self.assertEqual("ABCD", self.sol.convert(s = "ABCD", numRows = 1))
+
 if __name__ == '__main__':
     if False:
         unittest.main()
     else:
-        pass
+        sol = Solution()
+        sol.convert(s = "ABCD", numRows = 1)
+        # sol.convert(s = "123456789", numRows = 3)
