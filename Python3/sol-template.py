d8aa5b0afb90479f911b46dbc9265662e7e8459f
voitenko-lex <3534286+voitenko-lex@users.noreply.github.com>
2020-05-29 11:49:53 +0300

    created template for py3 solutions

---
+++ Python3/sol-template.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+### Description
+
+"""
+
+import unittest
+from typing import List, Set, Tuple, Dict
+# import collections
+
+class Solution:
+    def foo(self, num: int) -> List[int]:
+        result = 0
+        return result
+
+
+class TestMethods(unittest.TestCase):
+    sol = Solution()
+
+    def test_sample00(self):
+        test_input = self.sol.foo(2)
+        test_output = [0,1]
+        self.assertEqual(test_output, test_input)
+
+
+if __name__ == '__main__':
+    if False:
+        unittest.main()
+    else:
+        sol = Solution()
+        print(sol.foo(20))
+
+
+
